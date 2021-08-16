647594f2ae3c33e152176fe7fdfeeb90276e8cba
DarkStark9608 <65640108+DarkStark9608@users.noreply.github.com>
2021-08-16 12:50:24 -0500

    Create home_page.dart

---
+++ home_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          label: Text('Login'),
+          icon: Icon(Icons.login_outlined, color: Theme.of(context).primaryColor),
+          onPressed: () {
+            Navigator.pushNamed(context, 'login');
+          }),
+    );
+  }
+}
+
